2be3aeed7e0e9f708714f3a39f4e590ec7f67635
Tanmoy Paul <144669869+tanmoy230401@users.noreply.github.com>
2025-12-18 18:48:15 +0600

    Add files via upload

---
+++ MInimum_path_SUM.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int func(int n,int m,vector<vector<int>>& grid,vector<vector<int>>& dp)
+{
+    if(n == 0 and m ==0) return grid[0][0];
+    if(n<0 or m<0) return 1e6;
+    if(dp[n][m]!=-1) return dp[n][m];
+
+    int up = grid[n][m] + func(n-1,m,grid,dp);
+    int left = grid[n][m] + func(n,m-1,grid,dp);
+    return dp[n][m] = min(up,left);
+}
+    int minPathSum(vector<vector<int>>& grid) {
+       int n = grid.size();
+       int m = grid[0].size();
+       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+       return func(n-1,m-1,grid,dp); 
+    }
+};
